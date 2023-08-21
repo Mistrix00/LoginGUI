e0cca7643fadd22f4100b19cc9b94e98f7665cea
Mistrix <124061101+Mistrix00@users.noreply.github.com>
2023-08-21 16:48:03 +0200

    Add files via upload

---
+++ LoginApp/src/constants/CommonConstants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class CommonConstants {
+    public final static String APP_NAME= "Login Application GUI";
+
+    public final static int[] FRAME_SIZE = {400, 400};
+
+    public static final int TEXTFIELD_SIZE = 15;
+}
